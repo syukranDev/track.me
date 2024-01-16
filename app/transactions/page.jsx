ec3933cd8ea7776531d584b7e7533ab17228da30
syukranDev <syukran.dev@gmail.com>
2024-01-17 01:57:49 +0800

    fix

--- app/transactions/page.jsx
+++ app/transactions/page.jsx
@@ -8,7 +8,8 @@ import TablePagination from '../components/TablePagination'
 import axios from 'axios'
 import { useAuth } from '@clerk/nextjs';
 import { useSearchParams } from 'next/navigation'
-// import '@radix-ui/themes/styles.css';
+import '../../app/copyRadix.css';
+
 
 const page = () => {
     const { isLoaded, userId, sessionId, getToken } = useAuth();
